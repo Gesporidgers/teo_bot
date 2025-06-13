072c24936c64456659362abf0c619204a2d31712
Gesporidgers <49988107+Gesporidgers@users.noreply.github.com>
2025-06-13 15:30:56 +0300

    Создана функция для расшифровки символики Кендалла

--- FUN.py
+++ FUN.py
@@ -11,3 +11,32 @@ def Encryption(message):
     (pub, priv) = rsa.newkeys(256)
     msg_utf8 = message.encode('utf-8')
     return rsa.encrypt(msg_utf8,pub).hex()
+
+def Kendall(message: str):
+    smo = message.split('/')
+    Result = ""
+    SMO_dict = {
+        "M" : "Экспоненциальному закону",
+        "G" : "Произвольному закону общего вида",
+        "U" : "Равномерному закону",
+        "D" : "Детерминированному закону"
+    }
+    not_constant = {
+        "E" : "Закону Эрланга",
+        "h" : "Гипоэкспоненциальному закону",
+        "H" : "Гиперэкспоненциальному закону"
+    }
+    if smo[0] in SMO_dict:
+        Result += f"Входной поток распределён по *{SMO_dict[smo[0]]}*\n\n"
+    else:
+        Result += f"Входной поток распределён по *{not_constant[smo[0][0]]} {smo[0][1]} порядка*\n\n"
+    if smo[1] in SMO_dict:
+        Result += f"Выходной поток распределён по *{SMO_dict[smo[1]]}*\n\n"
+    else:
+        Result += f"Выходной поток распределён по *{not_constant[smo[1][0]]} {smo[1][1]} порядка*\n\n"
+    Result += f"СМО имеет _{smo[2]}_ каналов\n\n"
+    if len(smo) == 3:
+        Result += "У СМО неограниченная очередь"
+    else:
+        Result += f"СМО имеет {smo[3]} мест в очереди"
+    return Result
--- main.py
+++ main.py
@@ -8,7 +8,8 @@ markup_main = types.ReplyKeyboardMarkup(resize_keyboard=True)
 btn1 = types.KeyboardButton('Рассчитать Slope')
 btn2 = types.KeyboardButton('Ссылка на simbrief')
 btn3 = types.KeyboardButton('Сделать пароль')
-markup_main.add(btn1,btn2,btn3)
+btn4 = types.KeyboardButton('Расшифровать символику Кендалла')
+markup_main.add(btn1,btn2,btn3,btn4)
 current_mode = 0
 
 @bot.message_handler(commands=['start'])
@@ -33,6 +34,9 @@ def get_text_messages(message):
         elif message.text == 'Сделать пароль':
             bot.send_message(message.from_user.id,'Отправьте что нужно зашифровать',reply_markup=markup_sub)
             current_mode = 3
+        elif message.text == 'Расшифровать символику Кендалла':
+            bot.send_message(message.from_user.id, 'Отправьте характеристики СМО', reply_markup=markup_sub)
+            current_mode = 4
         elif message.text == 'Назад':
             current_mode = 0
             bot.send_message(message.from_user.id,'Привет',reply_markup=markup_main)
@@ -47,6 +51,8 @@ def get_text_messages(message):
     elif current_mode == 3:
         bot.send_message(message.from_user.id,'`'+Encryption(message.text)+'`',reply_markup=markup_main,parse_mode='MARKDOWN')
         current_mode = 0
-
+    elif current_mode == 4:
+        bot.send_message(message.from_user.id, Kendall(str(message.text)), reply_markup=markup_main,parse_mode='MARKDOWN')
+        current_mode = 0
 
 bot.polling(non_stop=True,interval=0)
